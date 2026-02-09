b3c63646afc0dff546a40e68845554fb9a786f49
transontri2006 <transontri2006.fpt@gmail.com>
2026-02-09 22:18:52 +0700

    Update Nhieubaigoplai.java

--- src/nhieubaigoplai/Nhieubaigoplai.java
+++ src/nhieubaigoplai/Nhieubaigoplai.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package nhieubaigoplai;
 
 import java.util.ArrayList;
@@ -131,4 +128,5 @@ public class Nhieubaigoplai {
         System.out.println("Tong: " + sum);
     }
 }
+
 
